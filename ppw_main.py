2fe89576dc0e3258547f673a68a0f483037fa4ee
ranianuraini <112038719+ranianuraini@users.noreply.github.com>
2023-12-11 12:19:45 +0700

    ppw_main.py

--- ppw_main.py
+++ ppw_main.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, jsonify
+import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 import re
 from nltk.tokenize import word_tokenize
-
-app = Flask(__name__)
 
 # Load the preprocessed data
 data = pd.read_csv('DataOlah_Antara.csv')
@@ -44,17 +42,25 @@ def clean_and_tokenize(text):
     tokens = stemmer.stem(' '.join(tokens)).split(' ')
     return ' '.join(tokens)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        data = request.get_json(force=True)
-        text = data['text']
-        cleaned_text = clean_and_tokenize(text)
-        text_tfidf = vectorizer.transform([cleaned_text])
-        prediction = nb_classifier.predict(text_tfidf)[0]
-        return jsonify({'prediction': prediction})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+def predict(text):
+    cleaned_text = clean_and_tokenize(text)
+    text_tfidf = vectorizer.transform([cleaned_text])
+    prediction = nb_classifier.predict(text_tfidf)[0]
+    return prediction
+
+def main():
+    st.title('Naive Bayes Text Classification App')
+
+    # Input text from user
+    user_input = st.text_area('Enter text for classification:', '')
+
+    # Make prediction when the user clicks the "Predict" button
+    if st.button('Predict'):
+        if user_input:
+            result = predict(user_input)
+            st.success(f'Prediction: {result}')
+        else:
+            st.warning('Please enter text for classification.')
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    main()
