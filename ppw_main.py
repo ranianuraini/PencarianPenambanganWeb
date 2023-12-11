f2ba817ea2cd4d5fdad4d926480a98287b224373
ranianuraini <112038719+ranianuraini@users.noreply.github.com>
2023-12-11 17:18:16 +0700

    ppw_main.py

--- ppw_main.py
+++ ppw_main.py
@@ -1,66 +1,89 @@
+# Install streamlit jika belum terinstal
+# pip install streamlit
+
 import streamlit as st
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-import re
-from nltk.tokenize import word_tokenize
+import requests
+from bs4 import BeautifulSoup
 
-# Load the preprocessed data
-data = pd.read_csv('DataOlah_Antara.csv')
-data.dropna(inplace=True)
+def scrape_data(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    first_page = soup.findAll('li', "dropdown mega-full menu-color1")
 
-# Separate features (X) and labels (y)
-X = data['artikel_tokens']
-y = data['Label']
+    save_categories = []
+    for links in first_page:
+        category = links.find('a').get('href')
+        save_categories.append(category)
 
-# Create a TF-IDF vectorizer
-vectorizer = TfidfVectorizer()
-X_tfidf = vectorizer.fit_transform(X)
+    category_search = [save_categories[5], save_categories[6], save_categories[9]]
+    datas = []
 
-# Create a Naive Bayes classifier
-nb_classifier = MultinomialNB()
-nb_classifier.fit(X_tfidf, y)
+    for ipages in range(1, 25):
+        for category in category_search:
+            response_news = requests.get(category + "/" + str(ipages))
+            name_category = category.split("/")
+            soup_news = BeautifulSoup(response_news.text, 'html.parser')
+            pages_news = soup_news.findAll('article', {'class': 'simple-post simple-big clearfix'})
 
-# Create a Sastrawi stemmer
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
+            for items in pages_news:
+                get_link_in = items.find("a").get("href")
+                response_article = requests.get(get_link_in)
+                soup_article = BeautifulSoup(response_article.text, 'html.parser')
 
-def clean_and_tokenize(text):
-    text = re.compile('<.*?>').sub('', str(text))
-    text = text.lower().strip()
-    text = re.compile('<.*?>').sub('', text)
-    text = re.compile('[%s]' % re.escape(string.punctuation)).sub(' ', text)
-    text = re.sub('\s+', ' ', text)
-    text = re.sub(r'\[[0-9]*\]', ' ', text)
-    text = re.sub(r'[^\w\s]', '', str(text).lower().strip())
-    text = re.sub(r'\d', ' ', text)
-    text = re.sub(r'\s+', ' ', text)
-    text = re.sub('nan', '', text)
-    tokens = word_tokenize(text)
-    tokens = [w for w in tokens if not w in stop_words]
-    tokens = stemmer.stem(' '.join(tokens)).split(' ')
-    return ' '.join(tokens)
+                check_title = soup_article.findAll("h1", "post-title")
+                if check_title:
+                    title = soup_article.find("h1", "post-title").text
+                else:
+                    title = ""
 
-def predict(text):
-    cleaned_text = clean_and_tokenize(text)
-    text_tfidf = vectorizer.transform([cleaned_text])
-    prediction = nb_classifier.predict(text_tfidf)[0]
-    return prediction
+                label = name_category[-1]
+
+                try:
+                    date = soup_article.find("span", "article-date").text
+                except AttributeError:
+                    date = "Data tanggal tidak ditemukan"
+
+                check_read_more = soup_article.findAll("span", "baca-juga")
+                trash1 = ""
+                if check_read_more:
+                    for read_more in check_read_more:
+                        text_trash = read_more.text
+                        trash1 += text_trash + ' '
+                else:
+                    trash1 = ""
+
+                articles = soup_article.find_all('div', {'class': 'post-content clearfix'})
+                if articles:
+                    article_content = soup_article.find('div', {'class': 'post-content clearfix'}).text
+                    article = article_content.replace("\n", " ").replace("\t", " ").replace("\r", "") \
+                        .replace(trash1, "").replace("\xa0", "")
+                else:
+                    article = ""
+
+                check_author = soup_article.findAll("p", "text-muted small mt10")
+                if check_author:
+                    author = soup_article.find("p", "text-muted small mt10").text.replace("\t\t", " ")
+                else:
+                    author = ""
+
+                datas.append({
+                    'Tanggal': date,
+                    'Penulis': author,
+                    'Judul': title,
+                    'Artikel': article,
+                    'Label': label
+                })
+
+    return datas
 
 def main():
-    st.title('Naive Bayes Text Classification App')
+    st.title("Aplikasi Berita Streamlit")
 
-    # Input text from user
-    user_input = st.text_area('Enter text for classification:', '')
+    url = "https://www.antaranews.com/"
+    datas = scrape_data(url)
 
-    # Make prediction when the user clicks the "Predict" button
-    if st.button('Predict'):
-        if user_input:
-            result = predict(user_input)
-            st.success(f'Prediction: {result}')
-        else:
-            st.warning('Please enter text for classification.')
+    st.write("## Data Berita")
+    st.write(datas)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
